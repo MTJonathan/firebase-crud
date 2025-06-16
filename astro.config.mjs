6e9ca3286bc4d4047fb863e15d3becdee9a9e57d
MTJonathan <jonathan02330@gmail.com>
2025-06-15 23:50:22 -0500

    feat: Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  output: 'server',
+  adapter: netlify()
 });
